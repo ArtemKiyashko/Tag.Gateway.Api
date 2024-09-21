a1ab9331b5563362389680d4e3d64fe895de64f0
Artem Kiyashko <artem86@hotmail.co.uk>
2024-09-21 20:38:03 +0200

    Use unique session id by default

--- Tag.Gateway.Repositories/TgMessageExtensions.cs
+++ Tag.Gateway.Repositories/TgMessageExtensions.cs
@@ -10,6 +10,6 @@ public static class TgMessageExtensions
         {
             UpdateType.Message => update.Message.Chat.Id.ToString(),
             UpdateType.EditedMessage => update.EditedMessage.Chat.Id.ToString(),
-            _ => 0.ToString(),
+            _ => Guid.NewGuid().ToString(),
         };
 }
